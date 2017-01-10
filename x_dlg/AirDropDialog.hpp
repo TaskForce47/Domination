3493eb6b87705d09ff4f218625f191dbd1278273
Pierre Humberdroz <p.humberdroz@gmail.com>
2017-01-10 07:05:01 +0100

    get ready for some mikero

--- x_dlg/AirDropDialog.hpp
+++ x_dlg/AirDropDialog.hpp
@@ -5,8 +5,64 @@ class D_AirDropDialog {
 	onUnLoad = "uiNamespace setVariable ['D_AirDropDialog', nil];d_airdrop_dialog_open = false";
 	effectTilesAlpha = 0.15;
 	class controlsBackground {
-		COMMON_BACKGROUND_VIGNETTE
-		COMMON_BACKGROUND_TILES
+		class Vignette: RscVignette {
+			idc = 114998;
+		};
+		class TileGroup: RscControlsGroupNoScrollbars {
+	  idc = 115099;
+	  x = safezoneX;
+	  y = safezoneY;
+	  w = safezoneW;
+	  h = safezoneH;
+	  disableCustomColors = 1;
+	  class Controls {
+		    class TileFrame: RscFrame
+		    {
+		      idc = 114999;
+		      x = 0;
+		      y = 0;
+		      w = safezoneW;
+		      h = safezoneH;
+		      colortext[] = {0,0,0,1};
+		    };
+		    BCGTILE(0,0)
+		    BCGTILE(0,1)
+		    BCGTILE(0,2)
+		    BCGTILE(0,3)
+		    BCGTILE(0,4)
+		    BCGTILE(0,5)
+		    BCGTILE(1,0)
+		    BCGTILE(1,1)
+		    BCGTILE(1,2)
+		    BCGTILE(1,3)
+		    BCGTILE(1,4)
+		    BCGTILE(1,5)
+		    BCGTILE(2,0)
+		    BCGTILE(2,1)
+		    BCGTILE(2,2)
+		    BCGTILE(2,3)
+		    BCGTILE(2,4)
+		    BCGTILE(2,5)
+		    BCGTILE(3,0)
+		    BCGTILE(3,1)
+		    BCGTILE(3,2)
+		    BCGTILE(3,3)
+		    BCGTILE(3,4)
+		    BCGTILE(3,5)
+		    BCGTILE(4,0)
+		    BCGTILE(4,1)
+		    BCGTILE(4,2)
+		    BCGTILE(4,3)
+		    BCGTILE(4,4)
+		    BCGTILE(4,5)
+		    BCGTILE(5,0)
+		    BCGTILE(5,1)
+		    BCGTILE(5,2)
+		    BCGTILE(5,3)
+		    BCGTILE(5,4)
+		    BCGTILE(5,5)
+		  };
+		};
 		__DDIALOG_BG($STR_DOM_MISSIONSTRING_1228)
 	};
 	class controls {
--- x_dlg/MarkArtilleryDialog.hpp
+++ x_dlg/MarkArtilleryDialog.hpp
@@ -1,12 +1,68 @@
 class D_MarkArtilleryDialog {
 	idd = -1;
-	movingEnable = true;
+	movingEnable = 1;
 	onLoad = "uiNamespace setVariable ['D_MarkArtilleryDialog', param [0]];[param [0]] call bis_fnc_guiEffectTiles;call d_fnc_initMarkArtyDlg;d_markarti_dialog_open = true";
 	onUnLoad = "uiNamespace setVariable ['D_MarkArtilleryDialog', nil];d_markarti_dialog_open = false";
 	effectTilesAlpha = 0.15;
 	class controlsBackground {
-		COMMON_BACKGROUND_VIGNETTE
-		COMMON_BACKGROUND_TILES
+		class Vignette: RscVignette {
+			idc = 114998;
+		};
+		class TileGroup: RscControlsGroupNoScrollbars {
+	  idc = 115099;
+	  x = safezoneX;
+	  y = safezoneY;
+	  w = safezoneW;
+	  h = safezoneH;
+	  disableCustomColors = 1;
+	  class Controls {
+		    class TileFrame: RscFrame
+		    {
+		      idc = 114999;
+		      x = 0;
+		      y = 0;
+		      w = safezoneW;
+		      h = safezoneH;
+		      colortext[] = {0,0,0,1};
+		    };
+		    BCGTILE(0,0)
+		    BCGTILE(0,1)
+		    BCGTILE(0,2)
+		    BCGTILE(0,3)
+		    BCGTILE(0,4)
+		    BCGTILE(0,5)
+		    BCGTILE(1,0)
+		    BCGTILE(1,1)
+		    BCGTILE(1,2)
+		    BCGTILE(1,3)
+		    BCGTILE(1,4)
+		    BCGTILE(1,5)
+		    BCGTILE(2,0)
+		    BCGTILE(2,1)
+		    BCGTILE(2,2)
+		    BCGTILE(2,3)
+		    BCGTILE(2,4)
+		    BCGTILE(2,5)
+		    BCGTILE(3,0)
+		    BCGTILE(3,1)
+		    BCGTILE(3,2)
+		    BCGTILE(3,3)
+		    BCGTILE(3,4)
+		    BCGTILE(3,5)
+		    BCGTILE(4,0)
+		    BCGTILE(4,1)
+		    BCGTILE(4,2)
+		    BCGTILE(4,3)
+		    BCGTILE(4,4)
+		    BCGTILE(4,5)
+		    BCGTILE(5,0)
+		    BCGTILE(5,1)
+		    BCGTILE(5,2)
+		    BCGTILE(5,3)
+		    BCGTILE(5,4)
+		    BCGTILE(5,5)
+		  };
+		};
 		__DDIALOG_BG($STR_DOM_MISSIONSTRING_1524)
 	};
 	class controls {
@@ -24,7 +80,7 @@ class D_MarkArtilleryDialog {
 			x = 0.685; y = 0.5;
 			w = 0.288; h = 0.035;
 			sizeEx = 0.032;
-		};		
+		};
 		class ArtyTypeCombo:FireSeriesCombo {
 			idc = 888;
 			y = 0.185;
@@ -56,7 +112,7 @@ class D_MarkArtilleryDialog {
 			colorBackground[] = {0.9, 0.9, 0.9, 0.9};
 			x = 0.03; y = 0.07;
 			w = 0.62; h = 0.85;
-			showCountourInterval = false;
+			showCountourInterval = 0;
 		};
 		__CANCELCLOSEB(-1)
 	};
